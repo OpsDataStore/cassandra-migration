d6580d7466b918808023571b1083282cb076a8f0
Joshua Goldie <josh@opsdatastore.com>
2017-05-16 15:33:10 -0400

    Testing with session that reconnects after every statement in multi-stmt cql migration

--- src/main/java/com/contrastsecurity/cassandra/migration/script/CqlScript.java
+++ src/main/java/com/contrastsecurity/cassandra/migration/script/CqlScript.java
@@ -110,6 +110,11 @@ public class CqlScript {
             if (!rs.getExecutionInfo().isSchemaInAgreement()) {
                 LOG.error("Schemas not in agreement after executing CQL: " + cqlStatement);
             }
+            //Test -- not sure this is staying
+            LOG.debug("Closing and reopening session...");
+            Cluster c = localSession.getCluster();
+            localSession.close();
+            localSession = c.connect();
         }
     }
 
